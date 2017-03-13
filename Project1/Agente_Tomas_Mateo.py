54393c90253c9ff11f04159d7dfe819f74efc543
Tomás Felipe Llano Ríos <tllano11@gmail.com>
2017-03-13 01:21:09 -0500

    Finishing project1: Updating files to start deploying on Jupyter.

--- Project1/Agente_Tomas_Mateo.py
+++ Project1/Agente_Tomas_Mateo.py
@@ -1,12 +1,13 @@
 import copy
-import eval_fun
+from eval_fun import evaluate
+import sys
 
-DEPTH_LIMIT = 98
+DEPTH_LIMIT = 3
 BOARD_SIZE = 121
 
 '''
   Determines which positions are free to make a move and
-  returns a list containing them.  
+  returns a list containing them.
 '''
 def get_available_moves(game_state):
   children = []
@@ -29,11 +30,13 @@ def get_min_value(player, game_state, depth, alpha, beta):
       chip = 2
       enemy = 1
     if depth == DEPTH_LIMIT:
-        return eval_fun.evaluate(player, game_state)
+        return evaluate(player, game_state)
     for move in moves:
         following = copy.deepcopy(game_state)
         following[move] = chip
         score = get_max_value(enemy, following, depth+1, alpha, beta)
+        if score is None:
+          score = float('inf')
         if score < best_score:
             best_move = move
             best_score = score
@@ -54,11 +57,13 @@ def get_max_value(player, game_state, depth, alpha, beta):
       chip = 2
       enemy = 1
     if depth == DEPTH_LIMIT:
-        return eval_fun.evaluate(player, game_state)
+        return evaluate(player, game_state)
     for move in moves:
         following = copy.deepcopy(game_state)
         following[move] = chip
         score = get_min_value(enemy, following, depth+1, alpha, beta)
+        if score is None:
+          score = float('-inf')
         if score > best_score:
             best_move = move
             best_score = score
@@ -70,7 +75,7 @@ def get_max_value(player, game_state, depth, alpha, beta):
   Obtains the minimax value from a given game state and
   returns the best possible move to make.
   Note that the "get_max_value" function is called after
-  iterating according to the "get_min_value" function's 
+  iterating according to the "get_min_value" function's
   structure (i.e. a get_min_value like iteration was made
   before calling get_max_value). It's done that way due to
   our evaluation function return value (Dijkstra's algoritm
@@ -90,8 +95,10 @@ def minimax(player, game_state, depth, alpha, beta):
     following = copy.deepcopy(game_state)
     following[move] = chip
     score = get_max_value(enemy, following, depth+1, alpha, beta)
+    if score is None:
+      continue
     if score < best_score:
-      best_move = following
+      best_move = move
       best_score = score
     if best_score < beta and beta <= best_score:
       beta = best_score
@@ -99,17 +106,75 @@ def minimax(player, game_state, depth, alpha, beta):
   return best_move
 
 '''
-  A pseudo "main"-like function called when it is desired to 
+  Obtains the values of the rows in order to return an answer to jupyter
+'''
+def get_row(it):
+  if it in range(0, 11):
+    return 0
+  elif it in range(11, 22):
+    return 1
+  elif it in range(22, 33):
+    return 2
+  elif it in range(33, 44):
+    return 3
+  elif it in range(44, 55):
+    return 4
+  elif it in range(55, 66):
+    return 5
+  elif it in range(66, 77):
+    return 6
+  elif it in range(77, 88):
+    return 7
+  elif it in range(88, 99):
+    return 8
+  elif it in range(99,110):
+    return 9
+  else:
+    return 10
+
+'''
+  Obtains the values of the columns in order to return an answer to jupyter
+'''
+def get_column(it, row):
+  if row == 0:
+    return it
+  if row == 1:
+    return it - 11
+  if row == 2:
+    return it - 22
+  if row == 3:
+    return it - 33
+  if row == 4:
+    return it - 44
+  if row == 5:
+    return it - 55
+  if row == 6:
+    return it - 66
+  if row == 7:
+    return it - 77
+  if row == 8:
+    return it - 88
+  if row == 9:
+    return it - 99
+  if row == 10:
+    return it - 110
+
+'''
+  A pseudo "main"-like function called when it is desired to
   to get the best possible move by using the minimax algorithm.
 '''
-def main(player, game_state):
-  game_state = [item for sublist in game_state for item in game_state]
+def Agente_Tomas_Mateo(player, game_state):
+  game_state = [item for sublist in game_state for item in sublist]
   result =  minimax(player, game_state, 0, float('-inf'), float('inf'))
-  tmp = []
-  result_matrix = []
-  for i in range(0, 121):
-    tmp.append(result[i])     # Append to make the temporal list that will be added to the final matrix as a result
-    if (i%11)-1 == 9:
-      result_matrix.append(tmp)   # Append to the temp list to give us a result
-      tmp = []                    # tmp gets empty to start over
-  return result_matrix
+  if result == []:
+    for i in range(len(game_state)):
+      if game_state[i] == 0:
+          row = get_row(i)
+          column = get_column(i, row)
+          position = [row, column]
+          return position
+
+  row = get_row(result)
+  column = get_column(result, row)
+  position = [row, column]
+  return position
--- Project1/eval_fun.py
+++ Project1/eval_fun.py
@@ -7,18 +7,18 @@ def dijkstra(player, adjacent, costs, start, end):
     predecessor = {}
     visited_set = set([start])
     step_count = 1
-    
+
     for i in adjacent.get(start, []):
         min_dist[i] = costs[start, i]
         item = [min_dist[i], start, i]
         heapq.heappush(prior_queue, item)
         distance_queue[i] = item
-    
+
     while prior_queue:
         cost, parent, left = heapq.heappop(prior_queue)
         if (step_count >= 10) and is_terminal_node(player, left):
           end = left
-          
+
         if left not in visited_set:
             predecessor[left] = parent
             visited_set.add(left)
@@ -38,15 +38,15 @@ def dijkstra(player, adjacent, costs, start, end):
                     distance_queue[i] = item
         step_count = step_count + 1
     return None
-  
+
     '''
-      Determines whether Dijkstra's algorithm has reached its goal, 
-      which is variant, or must keep searching. 
+      Determines whether Dijkstra's algorithm has reached its goal,
+      which is variant, or must keep searching.
       Example:
-        Suppose it was given to us a hex board (formatted as a flatten matrix, i.e. a list) 
+        Suppose it was given to us a hex board (formatted as a flatten matrix, i.e. a list)
         in which position number 10 is occupied by our enemy and we are playing as player 2,
         such as board[10] = 1. Therefore, the board's cost dictionary would contain:
-        {..., (9, 10):Inf, (20, 10):Inf, (21, 10):Inf, ...} and there would be 120 free 
+        {..., (9, 10):Inf, (20, 10):Inf, (21, 10):Inf, ...} and there would be 120 free
         positions in which to make a move.
         The get_start_position(2, board) function would return '0', as it is the first possible
         position in which to make a move and "end" would be set to 10 (read the "evaluation" function
@@ -62,15 +62,15 @@ def is_terminal_node(player, left):
     return left in range(110, 121)
   else:                              # Checks if left is one of the board's right position
     return ((left%11)-1) == 9
-  
+
   '''
     Determines which position is suitable to be Dijkstra's algorithm starting node
-    based on which player is playing. 
-    If player 1 is playing, then only the board's top positions (0, 1, 2, ... , 10) 
+    based on which player is playing.
+    If player 1 is playing, then only the board's top positions (0, 1, 2, ... , 10)
     are considered; otherwise it is assumed player 2 is playing and only the board's
     right positions (0, 11, 22, ... , 110) are considered.
   '''
-def get_start_position(player, game_state):  
+def get_start_position(player, game_state):
   if player == 1:
     if game_state[0] == 0:
       return 0
@@ -92,7 +92,7 @@ def get_start_position(player, game_state):
       return 8
     elif game_state[9] == 0:
       return 9
-    elif game_state[10] == 0:
+    else:
       return 10
   else:
     if game_state[0] == 0:
@@ -112,16 +112,16 @@ def get_start_position(player, game_state):
     elif game_state[77] == 0:
       return 77
     elif game_state[88] == 0:
-      return 88  
+      return 88
     elif game_state[99] == 0:
       return 99
-    elif game_state[110] == 0:
+    else:
       return 110
 
   '''
     Determines an specific move's near positions based on which player
     if playing.
-  '''   
+  '''
 def get_neighbours(move):
   if move%11 == 0:             # Left side of the board = 0, 11, 22, 33, ..., 110
     neighbours = [move - 11,
@@ -139,9 +139,9 @@ def get_neighbours(move):
                   move - 1,
                   move + 1,
                   move + 10,
-                  move + 11]    
+                  move + 11]
   return [n for n in neighbours if n > 0 and n < 121]
-  
+
   '''
     Based on the graph main concept of having a group of
     nodes, their adjacents and the cost of moving from
@@ -152,8 +152,8 @@ def get_neighbours(move):
   '''
 def get_graph(player, game_state):
   adjacents = {}
-  cost = {}    
-  
+  cost = {}
+
   if player == 1:
     cost_player1 = 2
     cost_player2 = float('inf')
@@ -172,19 +172,19 @@ def get_graph(player, game_state):
       else:                             # Player 2 has made a move in this position
         cost[(move, neighbour)] = cost_player2
   return adjacents, cost
-  
+
   '''
     Uses Dijkstra's algorithm to get a reliable heuristic regarding the
     minimum path cost to get from the top to the bottom of the board,
     if player 1, or the right to the left side of the board, if player 2.
     Remark:
       The algorithm works by iterating over a 11 x 11 flatten matrix (i.e. a list),
-      which means moving across rows is done by adding 11 to a given position 
-      (it also means 11 is the jump value between rows and 1 is the jump value 
+      which means moving across rows is done by adding 11 to a given position
+      (it also means 11 is the jump value between rows and 1 is the jump value
       between columns).
   '''
 def evaluate(player, game_state):
-  adjacents, cost = get_graph(player, game_state)  
+  adjacents, cost = get_graph(player, game_state)
   start = get_start_position(player, game_state)
   if player == 1:
     end = start + 110  # start + ((number_of_rows - 1) * jump_between_rows)
