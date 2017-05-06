d361aa271acfa0619991963a7a2e2d9942596bc8
Tomas Felipe Llano Rios <tllano11@gmail.com>
2017-05-06 13:05:29 -0400

    Adding matrix update method

--- Project2/agent_none.py
+++ Project2/agent_none.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from operator import itemgetter
 import random
 import numpy
@@ -17,12 +15,12 @@ RED = "rojo"
 
 class Node:
   
-  def __init__(self, id, value):
+  def __init__(self, id):
     self.id = id
     self.adjacents = self.__get_adjacents_from_id(id)
-    self.value = value
     self.prob_trans = 1/len(self.adjacents)
-  
+    self.value = None
+    
   def __get_adjacents_from_id(self, position):    
     if position == 0:
       return [position + 5, position + 1]
@@ -51,18 +49,75 @@ class Node:
     self.value = accum
 
 class AgentNone:
-
+  
   def __init__(self):
     self.player = None
     self.init_pos = None
-    self.hmm = self.__get_hmm(BOARD_SIZE)
+    self.ct = self.__get_color_table()
+    self.net = self.__get_net(BOARD_SIZE)
     self.probabilities = [random.random()]*25
-  
-  def __get_hmm(self, n_cells):
-    hmm = [None] * 25
-    for i in range(0, n_cells):
-      hmm[i] = Node(i, numpy.random.rand())
-    return hmm
+    self.__set_initial_probs()
+    
+  def __get_color_table(self):
+    return [[0.70, 0.15, 0.1, 0.05],
+            [0.17, 0.6, 0.17, 0.06],
+            [0.06, 0.17, 0.6, 0.17],
+            [0.05, 0.12, 0.23, 0.6],
+            [0.05, 0.1, 0.15, 0.8]]
+    
+  def __get_net(self, nodes):
+    net = []
+    for i in range(0, nodes):
+      net.append(Node(i))
+    return net
+    
+  def update_probs(self, measure, net, ct):
+    if measure == GREEN:
+      color = 0
+    elif measure == YELLOW:
+      color = 1
+    elif measure == ORANGE:
+      color = 3
+    elif measure == RED:
+      color = 4
+    for i in range(0, len(net)):
+      accum = 0
+      for j in range(0, len(net)):
+        tmp = abs(i-j)
+        if j==i:
+          accum += net[j].value * self.ct[0][color]
+        elif tmp == 1 or tmp == 5:
+          accum += net[j].value * self.ct[1][color]
+        elif tmp == 2 or tmp == 10:
+          accum += net[j].value * self.ct[2][color]
+        elif tmp == 3 or tmp == 15:
+          accum += net[j].value * self.ct[3][color]
+        else:
+          accum += net[j].value * self.ct[4][color]
+      net[i].value = accum
+      
+  def __set_initial_probs(self):
+    for i in range(0, len(self.net)):
+      prob_dist_0 = 1
+      prob_dist_1 = len(self.net[i].adjacents)
+      if prob_dist_1 == 4:
+        if i == 12:
+          prob_dist_2 = 8
+          prob_dist_3 = 8  
+        else:
+          prob_dist_2 = 7
+          prob_dist_3 = 7
+      elif prob_dist_1 == 3:
+        if i == 10 or i == 14 or i == 2 or i == 22:
+          prob_dist_2 = 5
+        else:
+          prob_dist_2 = 4
+        prob_dist_3 = 5
+      else:
+        prob_dist_2 = 3
+        prob_dist_3 = 4
+      prob_dist_4 = 25 - (prob_dist_0 + prob_dist_1 + prob_dist_2 + prob_dist_3)
+      self.net[i].value = (prob_dist_0*self.ct[0][0] + prob_dist_1*self.ct[1][0] + prob_dist_2*self.ct[2][0] + prob_dist_3*self.ct[3][0] + prob_dist_4*self.ct[4][0])/25
   
   def run_agent_none(self, current_player, result_enemy_action, enemy_action, init_pos):
     if self.player is None:
@@ -74,7 +129,7 @@ class AgentNone:
       pass
     elif enemy_action == MEASURE:
       if color == GREEN or color == YELLOW:
-        move(hmm[current_pos], val_measure)        #value of measure
+        move(net[current_pos], val_measure)
     elif enemy_action == MOVE:
       pass
  
@@ -126,14 +181,10 @@ class AgentNone:
     pass
   
 def main():
-  p = numpy.random.rand(25)
-  R = numpy.random.rand(3, 25)
-  
-  print("p: ", p)
-  print("R: ", R)
   a = AgentNone()
-  print("EVPI", a.get_evpi(R.tolist(), p.tolist(), 3, 25))
-  print(a.hmm[16].prob_trans)
+  print("ANTES: ",[x.value for x in a.net])
+  a.update_probs("amarillo", a.net, a.ct)
+  print("DESPUES: ",[x.value for x in a.net])
   
 if __name__ == "__main__":
   main()
