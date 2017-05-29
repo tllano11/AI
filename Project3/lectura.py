eae422ba1b2f63ee0f09e367cb0d0a68af8d4254
Tomás Felipe Llano Ríos <tllano11@gmail.com>
2017-05-29 18:07:48 -0500

    Enabling tweets processing.

--- Project3/lectura.py
+++ Project3/lectura.py
@@ -11,6 +11,7 @@ import nltk
 from nltk.stem import *
 from nltk.corpus import stopwords
 from textblob.classifiers import NaiveBayesClassifier
+from nltk.tokenize import RegexpTokenizer
 
 listaTweets = []
 listaTweets2 = []
@@ -23,47 +24,47 @@ def excel_reader(nombreArchivo,nombreArchivo2):
     for tweet in excel:
         listaTweets.append(tweet['Texto'])#.encode('utf-8'))          #Creacion de una lista con los tweets
     for tweet2 in excel:
-        listaTweets2.append(tweet['Texto'])
-    naiveBayes(listaTweets,listaTweets2)
-    #tokenize(listaTweets)
-    
-def tokenize(lista):
-    size = len(lista)
-    for i in range(0, size):
-        sentence = lista[i]
-        list_tokens.append(nltk.word_tokenize(sentence))
-    #porter_stemmer(list_tokens)
-    #snowball_stemmer(list_tokens)
-    #stop_words(list_tokens)
-    naiveBayes(list_tokens)
-    
+        listaTweets2.append(tweet2['Texto'])
+    #naiveBayes(listaTweets,listaTweets2)
+    selected_tweets = tokenize(listaTweets)
+    not_selected_tweets = tokenize(listaTweets2)
+
+def tokenize(tweets):
+    tokenizer = RegexpTokenizer("[a-zA-Z]+[a-zA-Z]+[a-zA-Z]*")
+    for tweet in tweets:
+        words = tokenizer.tokenize(tweet)
+        list_tokens.append(stop_words(words))
+    return list_tokens
+
 def porter_stemmer(list_tokens):
-    sentences = str(list_tokens)
     stemmer = PorterStemmer()
-    return [stemmer.stem(str(sentence)) for sentence in sentences]
+    return [stemmer.stem(sentence) for sentence in list_tokens]
+
+def stem_token(token):
+    stemmer = PorterStemmer()
+    return stemmer.stem(token)
 
 def snowball_stemmer(list_tokens):
     stemmer = SnowballStemmer("spanish")
     return stemmer.stem(str(list_tokens))
 
-def stop_words(list_tokens):
+def stop_words(words):
     filteredWords = []
     stopWords = set(stopwords.words('spanish'))
-    words = porter_stemmer(list_tokens)
+    stopWords.update(["https", "http", "RT", "co"])
+    for w in words:
+        if w not in stopWords:
+            filteredWords.append(stem_token(w))
+    return filteredWords
 
-    for i in words:
-        if i not in stopWords:
-            filteredWords.append(i)
-    print(filteredWords)
-    
 def naiveBayes(listaTweets, listaTweets2):
     train = [(str(listaTweets), "Classified")]
     test = [(str(listaTweets2), "Unclassified")]
     cl = NaiveBayesClassifier(train)
     print(cl.classify(str(test)))
-    
+
 def main(archivo,archivo2):
     excel_reader(archivo,archivo2)
-    
+
 if __name__ == "__main__":
     main("./files/Tweets_Seleccionados.xlsx","./files/Todos.xlsx")
