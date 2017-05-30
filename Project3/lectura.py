fb8fc1c6bd8f72c70d1be63b14beb630591cd53a
Mateo Guty <mateogutigomez@gmail.com>
2017-05-29 21:27:21 -0500

    Adding Tweet Features

--- Project3/lectura.py
+++ Project3/lectura.py
@@ -8,6 +8,7 @@ nltk.download('stopwords')
 
 import pyexcel as pe
 import nltk
+import numpy as np
 from nltk.stem import *
 from nltk.corpus import stopwords
 from textblob.classifiers import NaiveBayesClassifier
@@ -16,6 +17,7 @@ from nltk.tokenize import RegexpTokenizer
 listaTweets = []
 listaTweets2 = []
 list_tokens = []
+
 """Funcion para la lectura del archivo de excel con el campo texto predefinido"""
 
 def excel_reader(nombreArchivo,nombreArchivo2):
@@ -25,15 +27,18 @@ def excel_reader(nombreArchivo,nombreArchivo2):
         listaTweets.append(tweet['Texto'])#.encode('utf-8'))          #Creacion de una lista con los tweets
     for tweet2 in excel:
         listaTweets2.append(tweet2['Texto'])
+    features(listaTweets)
+    #tokenize(listaTweets)
     #naiveBayes(listaTweets,listaTweets2)
-    selected_tweets = tokenize(listaTweets)
-    not_selected_tweets = tokenize(listaTweets2)
-
+    #selected_tweets = tokenize(listaTweets)
+    #not_selected_tweets = tokenize(listaTweets2)
+    
 def tokenize(tweets):
     tokenizer = RegexpTokenizer("[a-zA-Z]+[a-zA-Z]+[a-zA-Z]*")
     for tweet in tweets:
         words = tokenizer.tokenize(tweet)
         list_tokens.append(stop_words(words))
+    #features(list_tokens)
     return list_tokens
 
 def porter_stemmer(list_tokens):
@@ -44,9 +49,9 @@ def stem_token(token):
     stemmer = PorterStemmer()
     return stemmer.stem(token)
 
-def snowball_stemmer(list_tokens):
-    stemmer = SnowballStemmer("spanish")
-    return stemmer.stem(str(list_tokens))
+# def snowball_stemmer(list_tokens):
+#     stemmer = SnowballStemmer("spanish")
+#     return stemmer.stem(str(list_tokens))
 
 def stop_words(words):
     filteredWords = []
@@ -57,11 +62,19 @@ def stop_words(words):
             filteredWords.append(stem_token(w))
     return filteredWords
 
-def naiveBayes(listaTweets, listaTweets2):
-    train = [(str(listaTweets), "Classified")]
-    test = [(str(listaTweets2), "Unclassified")]
+def features(lista):
+    tweets = np.array(tokenize(lista),dtype=object)
+    tweets = np.hstack(tweets.flat)
+    wordlist = nltk.FreqDist(tweets)
+    word_features,v = zip(*wordlist.most_common())
+    return word_features
+
+def naiveBayes(listaTweets):
+    train = [(str(tokenize(listaTweets)), "Classified")]
+    test = [(str(tokenize(listaTweets2)), "Unclassified")]
     cl = NaiveBayesClassifier(train)
-    print(cl.classify(str(test)))
+    #return cl.classify(str(test))
+    return train
 
 def main(archivo,archivo2):
     excel_reader(archivo,archivo2)
