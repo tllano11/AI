d893b012fa983be6a5129bccde1916992e560f22
Tomás Felipe Llano Ríos <tllano11@gmail.com>
2017-05-31 07:44:55 -0500

    Adding softmax to ann

--- Project3/ann.py
+++ Project3/ann.py
@@ -10,9 +10,9 @@ class ANN:
 
   def __init__(self, activation_function, error_function):
     #Define HyperParameters
-    self.input_layer_size = 2
-    self.output_layer_size = 1
-    self.hidden_layer_size = 3
+    self.input_layer_size = 114
+    self.output_layer_size = 2
+    self.hidden_layer_size = 10
     self.learning_rate = 0.1
     self.activation_function = activation_function
     self.error_function = error_function
@@ -23,6 +23,22 @@ class ANN:
     self.W2 = np.random.randn(self.hidden_layer_size,\
                               self.output_layer_size).astype(np.float128)
 
+  def classify(self, tweets):
+    inputs = self.get_input(tweets)
+    self.forward_propagate(inputs)
+    return self.prediction
+
+  def get_input(self, tweets):
+    inputs = []
+    for tweet in tweets:
+      bag = []
+      for word in self.bag_of_words:
+        bag.append(1) if word in tweet else bag.append(0)
+      inputs.append(bag)
+    input_size = len(inputs)
+    bog_size = len(self.bag_of_words)
+    return np.array(inputs, dtype=np.float128).reshape(input_size, bog_size)
+
   def forward_propagate(self, inputs):
     """Propagate input values through the network
 
@@ -32,11 +48,11 @@ class ANN:
     #Forwarded values from layers 1 to 2
     self.z2 = np.dot(inputs, self.W1).astype(np.float128)
     #Output values from layer 2
-    self.a2 = self.activate(self.z2, Functions.SIGMOID)
+    self.a2 = self.activate(self.z2, self.activation_function)
     #Forwarded values from layers 2 to 3
     self.z3 = np.dot(self.a2, self.W2).astype(np.float128)
     #Output from layer 3 (output layer)
-    self.prediction = self.activate(self.z3, Functions.SIGMOID)
+    self.prediction = self.activate(self.z3, self.activation_function)
 
   def activate(self, z, function):
     """Apply an activation function to the activities of a layer
@@ -47,6 +63,8 @@ class ANN:
     """
     if function == Functions.SIGMOID:
       return sigmoid(z)
+    elif function == Functions.SOFTMAX:
+      return softmax(z)
     else:
       print("Unrecognized activation function")
       sys.exit(1)
@@ -70,7 +88,7 @@ class ANN:
     #Update weights from layers 1 to 2
     self.W1 = self.W1 - (self.learning_rate * djdW1)
 
-  def train(self, inputs, expected_prediction, max_err):
+  def train(self, inputs, expected_prediction):
     """ Trains the neuronal network by updating its weights.
 
     Keyword arguments:
@@ -79,17 +97,15 @@ class ANN:
     max_err -- Expected maximum error for the network
     """
     self.forward_propagate(inputs)
-    error = self.get_error(Functions.MSE, expected_prediction)
-
-    while error > max_err:
-      self.back_propagate(expected_prediction, inputs)
-      self.forward_propagate(inputs)
-      error = self.get_error(Functions.MSE, expected_prediction)
+    self.back_propagate(expected_prediction, inputs)
+    self.forward_propagate(inputs)
+    self.error = self.get_error(Functions.MSE, expected_prediction)
 
   def get_training_set(self, selected_tweets, rejected_tweets):
     st = fe.extract_features(selected_tweets)
     nst = fe.extract_features(rejected_tweets)
     self.set_bag_of_words(st, nst)
+    bog_size = len(self.bag_of_words)
     inputs = []
     outs = []
 
@@ -106,6 +122,10 @@ class ANN:
         bag.append(1) if word in tweet else bag.append(0)
       inputs.append(bag)
       outs.append([0, 1])
+    input_size = len(inputs)
+    out_size = len(outs)
+    inputs = np.array(inputs, dtype=np.float128).reshape(input_size, bog_size)
+    outs = np.array(outs, dtype=np.float128).reshape(out_size, 2)
 
     return inputs, outs
 
@@ -117,7 +137,7 @@ class ANN:
     bag_selected = [k for k, v in words_selected_tweets]
     bag_rejected = [k for k, v in words_rejected_teets]
 
-    self.bag_of_words = bag_selected + bag_rejected
+    self.bag_of_words = bag_selected[:41] + bag_rejected[:73]
 
   def get_error(self, function, expected_prediction):
     """ Returns the net's total error.
@@ -128,11 +148,12 @@ class ANN:
     """
     if function == Functions.MSE:
       error = mse(expected_prediction, self.prediction)
-      return error.sum()
+      return error
 
   def get_delta3(self, expected_prediction):
     if self.error_function == Functions.MSE:
-      if self.activation_function == Functions.SIGMOID:
+      if self.activation_function == Functions.SIGMOID or\
+         self.activation_function == Functions.SOFTMAX:
         delta = np.multiply(mse_prime(expected_prediction, self.prediction),\
                             sigmoid_prime(self.prediction))
         return delta
@@ -142,7 +163,8 @@ class ANN:
 
   def get_delta2(self, delta3):
     if self.error_function == Functions.MSE:
-      if self.activation_function == Functions.SIGMOID:
+      if self.activation_function == Functions.SIGMOID or\
+         self.activation_function == Functions.SOFTMAX:
         delta = np.dot(delta3, self.W2.T) * sigmoid_prime(self.z2)
         return delta
     else:
--- Project3/functions.py
+++ Project3/functions.py
@@ -1,12 +1,13 @@
 import numpy as np
 from enum import IntEnum, auto
 
+
 class Functions(IntEnum):
   #Activation functions
   SIGMOID = auto()
+  SOFTMAX = auto()
   #Error functions
   MSE = auto()
-
 
 def sigmoid(z):
   """Apply sigmoid activation function
@@ -16,10 +17,13 @@ def sigmoid(z):
   """
   return 1/(1+np.exp(-z))
 
-
 def sigmoid_prime(prediction):
   return prediction * (1-prediction)
 
+def softmax(z):
+  """Compute softmax values for z"""
+  e_z = np.exp(z - np.max(z))
+  return e_z / e_z.sum(axis=0)
 
 def mse(expected_prediction, prediction):
   """Returns the Mean Squared Error of the net's output values
@@ -29,7 +33,6 @@ def mse(expected_prediction, prediction):
   prediction -- Matrix containing the net's output values
   """
   return ((expected_prediction-prediction)**2)/2
-
 
 def mse_prime(expected_prediction, prediction):
   return (prediction - expected_prediction)
